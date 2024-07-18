15cfc7c9150bd5d002a74a573f338ce5c2bc70fe
ratamranjith@gmail.com <ratamranjith@gmail.com>
2024-07-18 20:52:06 +0530

    simple modification adding vercel

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: VITE_REACT_APP_BACKEND_BASEURL,
         changeOrigin: true,
         secure: false,
       },
